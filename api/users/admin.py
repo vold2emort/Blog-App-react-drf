26d72da78daa6174c3bbc000a6315a649b050827
vold2emort <pramishkc066@gmail.com>
2026-09-17 12:12:42 +0545

    feat: add user model for

---
+++ api/users/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.db import models
+from django.forms import Textarea
+
+from users.models import CustomUser
+
+
+class UserAdminConfig(UserAdmin):
+    model = CustomUser
+    search_fields = (
+        "email",
+        "user_name",
+    )
+    list_filter = ("email", "user_name", "is_active", "is_staff")
+    ordering = ("-start_date",)
+    list_display = ("email", "user_name", "is_active", "is_staff")
+    fieldsets = (
+        (
+            None,
+            {"fields": ("email", "user_name")},
+        ),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+        ("Personal", {"fields": ("about",)}),
+    )
+    formfield_overrides = {
+        models.TextField: {"widget": Textarea(attrs={"rows": 20, "cols": 60})},
+    }
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "user_name",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, UserAdminConfig)
